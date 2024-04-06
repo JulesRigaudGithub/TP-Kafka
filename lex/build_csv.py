61a5fef4dc4b2864a2c06f04fe92936f32a58225
Jules Rigaud <jules.rigaud@student-cs.fr>
2024-04-07 00:26:52 +0200

    Modifications pour nettoyage des données lexicales

--- lex/build_csv.py
+++ lex/build_csv.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 df = pd.read_excel("lexique3/Lexique383.xlsb")
 
-df[["1_ortho", "3_lemme", "4_cgram"]].to_csv("lexique.csv", sep=',', index=False)
+df_clean = df[["1_ortho", "3_lemme", "4_cgram"]].dropna()
+df_clean.to_csv("lexique.csv", sep=',', index=False, header=False)
